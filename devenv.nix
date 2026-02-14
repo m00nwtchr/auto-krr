7d2feb2312c7fcae30bbe98420feff2e7d0ab4b5
m00nwtchr <m00nwtchr@duck.com>
2026-02-14 19:59:12 +0100

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+{
+  packages = [
+    pkgs.git
+    pkgs.codex
+    pkgs.ruff
+  ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    lsp.package = pkgs.ty;
+    uv.enable = true;
+    uv.sync.enable = true;
+  };
+
+  treefmt = {
+    enable = true;
+    config.programs = {
+      nixfmt.enable = true;
+      ruff-check.enable = true;
+    };
+  };
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks = {
+    treefmt.enable = true;
+  };
+}
